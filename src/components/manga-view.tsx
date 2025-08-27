8f334f82cf9459a68424dd1c60d9237bfda5d912
Code Kickoff <prithvipawar289@gmail.com>
2025-08-27 14:26:02 +0530

    modified app

--- src/components/manga-view.tsx
+++
@@ -1,3 +0,0 @@
-// This file is no longer used and can be removed.
-// The functionality has been replaced by the new Homepage component and will be
-// integrated differently based on the new design.
